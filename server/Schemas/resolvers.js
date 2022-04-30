e0d68d0c09b9a4a788184ea768bddb2a8300a2a7
brittany klose <brittany.klose@yahoo.com>
2022-04-30 12:34:06 -0400

    resolvers

--- server/Schemas/resolvers.js
+++ server/Schemas/resolvers.js
@@ -1,3 +1,6 @@
+//Define the query and mutation functionality to work with the Mongoose models.
+//Hint: Use the functionality in the user-controller.js as a guide.
+
 const { User} = require('../models');
 
 const resolvers = {
@@ -5,5 +8,24 @@ const resolvers = {
         user: async () => {
             return Tech.find({});
           },
-    }
+          matchups: async (parent, { _id }) => {
+            const params = _id ? { _id } : {};
+            return Matchup.find(params);
+          },
+    },
+    Mutation: {
+        createMatchup: async (parent, args) => {
+          const matchup = await Matchup.create(args);
+          return matchup;
+        },
+        createVote: async (parent, { _id, techNum }) => {
+          const vote = await Matchup.findOneAndUpdate(
+            { _id },
+            { $inc: { [`tech${techNum}_votes`]: 1 } },
+            { new: true }
+          );
+          return vote;
+        },
+      },
+}
 module.exports = resolvers;
