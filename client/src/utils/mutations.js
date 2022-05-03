8fe19b834911a4739a278a9ef555858b3cdee9f1
brittany klose <brittany.klose@yahoo.com>
2022-05-03 14:45:29 -0400

    mutations for get remove functions

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -4,9 +4,11 @@ import { gql } from '@apollo/client';
 export const LOGIN_USER =gql`
 mutation login($email: String!, $password: String!){
     login(email: $email, password: $password){
-        _id
-        email
-        password
+        token
+        user{
+          _id
+          username  
+        }   
     }
 }
 `;
@@ -14,29 +16,50 @@ mutation login($email: String!, $password: String!){
 //adduser
 export const ADD_USER =gql`
 mutation addUser($username: String!, $email: String!, $password: String!){
-    addUser(username:$username, email: $email, password: $password){
-        _id
-        username
-        email
-        password
+    addUser(username: $username, email: $email, password: $password){
+        token
+        user{
+            _id
+            username
+        }
     }
 }
 `;
 
 //savebook
 export const SAVE_BOOK=gql`
-mutation saveBook($){
-    saveBook(input: BookInput){
-
+mutation saveBook($book: BookInput!){
+    saveBook(book: $book){
+        username
+        email
+        bookcount
+        savedBooks{
+            bookId
+            title
+            authors
+            description
+            image
+            link
+        }
     }
 }
 `;
 
 //remove book
 export const REMOVE_BOOK=gql`
-mutation removeBook($bookId: ID!){
+mutation removeBook($bookId: String!){
     removeBook(bookId: $ bookId){
-        bookId
+        username
+        email
+        bookcount
+        savedBooks{
+            bookId
+            title
+            authors
+            description
+            image
+            link
+        }
     }
 }
 `;
