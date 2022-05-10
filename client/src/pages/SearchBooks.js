18ae11a5da2215e310788752921f883a2cfe050c
brittany klose <brittany.klose@yahoo.com>
2022-05-10 15:09:27 -0400

    minor changes to make build work

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -33,7 +33,7 @@ const SearchBooks = () => {
 
     try {
       // const response = await searchGoogleBooks(searchInput);
-      const reponse = await fetch(
+      const response = await fetch(
       `https://www.googleapis.com/books/v1/volumes?q=${searchInput}`
       );
 
