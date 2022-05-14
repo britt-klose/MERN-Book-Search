e2626cbeb92351b136012505edab1af058f3414e
brittany klose <brittany.klose@yahoo.com>
2022-05-14 12:44:49 -0400

    put in env var

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -2,7 +2,7 @@
 const jwt = require('jsonwebtoken');
 
 // set token secret and expiration date
-const secret = 'mysecretsshhhhh';
+const secret = process.env.secret;
 const expiration = '2h';
 
 module.exports = {
